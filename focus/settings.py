14bfe84dc7b6c988ca622a5107c9f16ca2225f9d
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-02 15:40:55 +0100

    added decoule

--- focus/settings.py
+++ focus/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,10 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&&#bpq-g=41_+cb)3j9u)9_d*m^4jjoxy#nn8b0xt3&y8fswo='
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
