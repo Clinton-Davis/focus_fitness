ec89ff47e742709d115e544bb17d192035bab077
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-03 17:09:01 +0100

    added about contact and index page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ def index(request):
     """Returns the index page"""
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """Returns the about page"""
+
+    return render(request, 'home/about.html')
+
+
+def contact(request):
+    """Returns the about page"""
+
+    return render(request, 'home/contact.html')
