482f64c5470ca1acc45de145b21d7bae91aa9a80
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-03 14:33:16 +0100

    added home app and templates

--- focus/settings.py
+++ focus/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 
@@ -50,7 +51,9 @@ ROOT_URLCONF = 'focus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
