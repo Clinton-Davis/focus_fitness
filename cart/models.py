5b2887b01adfc23f985b19f2a4fa47ea58b0e7bf
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-06 14:39:13 +0100

    added to admin/detailed product page and add to cart

--- cart/models.py
+++ cart/models.py
@@ -8,7 +8,6 @@ User = get_user_model()
 
 
 class Address(models.Model):
-
     ADDRESS_CHOICES = (
         ('B', 'Billing'),
         ('S', 'Shipping'),
@@ -18,7 +17,7 @@ class Address(models.Model):
     address_line_1 = models.CharField(max_length=150)
     address_line_2 = models.CharField(max_length=150)
     city = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
     address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
     default = models.BooleanField(default=False)
 
@@ -34,7 +33,6 @@ class Product(models.Model):
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to='product_images')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
@@ -42,15 +40,13 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    """Get absolute urls"""
-
     def get_absolute_url(self):
         return reverse("cart:product-detail", kwargs={'slug': self.slug})
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(
-        'Order', related_name='items', on_delete=models.CASCADE)
+        "Order", related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
@@ -59,23 +55,20 @@ class OrderItem(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField(blank=True, null=True)
     ordered = models.BooleanField(default=False)
 
     billing_address = models.ForeignKey(
-        Address, related_name='billing_address',
-        blank=True, null=True, on_delete=models.SET_NULL)
-
+        Address, related_name='billing_address', blank=True, null=True, on_delete=models.SET_NULL)
     shipping_address = models.ForeignKey(
-        Address, related_name='shipping_address',
-        blank=True, null=True, on_delete=models.SET_NULL)
+        Address, related_name='shipping_address', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.reference_number
 
-    """ Clearting a order number from PK """
     @property
     def reference_number(self):
         return f"ORDER-{self.pk}"
@@ -99,16 +92,10 @@ class Payment(models.Model):
     def reference_number(self):
         return f"PAYMENT-{self.order}-{self.pk}"
 
-    def __str__(self):
-        return self.order
 
-
-""" this is a signl to slug a product to make unique"""
-
-
-def pre_save_product_Reciever(sender, instance, *args, **kwargs):
+def pre_save_product_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = slugify(instance.title)
 
 
-pre_save.connect(pre_save_product_Reciever, sender=Product)
+pre_save.connect(pre_save_product_receiver, sender=Product)
