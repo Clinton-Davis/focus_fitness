bb3b546a9e00c40c80e58f720783e39aceee3758
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-08 21:39:00 +0100

    added product.html and template to prodkucts

--- focus/urls.py
+++ focus/urls.py
@@ -9,7 +9,8 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('products/', include('products.urls'))
 
 
 ]
