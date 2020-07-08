be36ab693b7aa7d23c9b6a985e3cf3a5dd66ceda
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-08 16:26:04 +0100

    added search but need to fix bugs

--- cart/admin.py
+++ cart/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, OrderItem, Order, SizeVariation
+from .models import (
+    Product,
+    OrderItem,
+    Order,
+    SizeVariation,
+    Address
+)
 
 admin.site.register(Product)
+admin.site.register(Address)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(SizeVariation)
--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.CartView.as_view(), name='summary'),
+
     path('shop/', views.ProductListView.as_view(), name='product-list'),
     path('shop/<slug>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('increase-quantity/<pk>/',
@@ -13,5 +14,6 @@ urlpatterns = [
          views.DecreaseQuantityView.as_view(), name='decrease-quantity'),
     path('remove-from-cart/<pk>/',
          views.RemoveFromCartView.as_view(), name='remove-from-cart'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout')
 
 ]
