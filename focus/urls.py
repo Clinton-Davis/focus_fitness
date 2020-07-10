8663694036be82777594aa938a2f132e4c59664d
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-10 15:21:38 +0100

    added created checkout app and modles

--- focus/urls.py
+++ focus/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    # path('checkout/', include('checkout.urls')),
 
     path('favicon.ico', RedirectView.as_view(
         url=staticfiles_storage.url('images/favicon.ico')))
