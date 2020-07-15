38dc7d7404df003ade32a0c0ebfa1967f33929ee
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-15 09:49:47 +0100

    fixed accountlogin links and linked to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import UserProfile
 
 
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
     """Displaying user Prodile """
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
