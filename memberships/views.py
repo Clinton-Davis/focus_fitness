20bafa65b9f985ebc5331201a2f981700fff23d8
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-18 12:07:43 +0100

    added models to fuctions  get membership_types

--- memberships/views.py
+++ memberships/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView
-from .models import Membership, UserMembership
+from .models import Membership, UserMembership, Subscription
 
 
 def get_user_membership(request):
@@ -12,6 +14,15 @@ def get_user_membership(request):
     return None
 
 
+def get_user_subscription(request):
+    user_subscription_qs = Subscription.objects.filter(
+        user_membership=get_user_membership(request))
+    if user_subscription_qs.exists():
+        user_subscription = user_subscription_qs.first()
+        return user_subscription
+    return None
+
+
 class MembershipSelectView(ListView):
     model = Membership
 
@@ -19,5 +30,29 @@ class MembershipSelectView(ListView):
         context = super().get_context_data(**kwargs)
         current_membership = get_user_membership(self.request)
         context['current_membership'] = str(current_membership.membership)
-        print(context)
         return context
+
+    def post(self, request, **kwargs):
+        selected_membership_type = request.POST.get('membership_type')
+
+        user_membership = get_user_membership(request)
+        user_subscription = get_user_subscription(request)
+
+        selected_membership_qs = Membership.objects.filter(
+            membership_type=selected_membership_type)
+
+        if selected_membership_qs.exists():
+            selected_membership = selected_membership_qs.first()
+
+        """ VALIDATION """
+
+        if user_membership.membership == selected_membership:
+            if user_subscription != None:
+                messages.info(request, "You have this membership. Your next payment is due \
+                    {}".format('get this from stripe'))
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        # assing selected_membership_type to session
+        request.session['selected_membership_type'] = selected_membership.membership_type
+
+        return HttpResponseRedirect(reverse('memberships:payment'))
