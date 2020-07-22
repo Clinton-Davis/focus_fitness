9e3e43333bb12afd70039ecab4d21cf8fb9a89a5
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-22 21:40:34 +0100

    deleted mirgrations and added friendly names

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -17,7 +18,8 @@ class Category(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        'Category', null=True, on_delete=models.SET_NULL)
     content = models.TextField()
     thumbnail = models.ImageField()
     publish_date = models.DateTimeField(auto_now_add=True)
