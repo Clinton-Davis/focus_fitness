d43129dd9487cb96cdaff47c973779827bf868e5
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-07-30 17:08:35 +0100

    added styling and functions to programs and workouts

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from home.views import AboutView, ContactView, IndexView
 
 urlpatterns = [
 
-    # path('', views.index, name='home'),
+
     path('', IndexView, name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/',  ContactView.as_view(), name='contact')
