0203a68f28220fe2485cc498a15be1e0c695cb32
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-08-03 09:37:18 +0100

    added auto-focus to forms contact me and stlyed card in css

--- home/forms.py
+++ home/forms.py
@@ -3,10 +3,17 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': "form-group aw", 'placeholder': "Your Name"
+        'autofocus': True,
+        'class': "form-group aw",
+        'placeholder': "Your Name",
+
     }))
     email = forms.EmailField(widget=forms.TextInput(attrs={
-        'class': "form-group aw", 'placeholder': "Your Email"
+        'class': "form-group aw",
+        'placeholder': "Your Email"
     }))
     message = forms.CharField(widget=forms.Textarea(attrs={
-        'class': "form-group aw", 'rows': "4", 'cols': "50", 'placeholder': "Your Message"}))
+        'class': "form-group aw",
+        'rows': "4",
+        'cols': "50",
+        'placeholder': "Your Message"}))
