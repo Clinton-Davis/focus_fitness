501fd04307645a124bc3d657b0195e5f5cb8ab44
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-08-08 14:29:40 +0100

    fixing marketing bug

--- marketing/views.py
+++ marketing/views.py
@@ -10,15 +10,15 @@ import requests
 import json
 
 
-MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
-MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
-MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
+# MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
+# MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
+# MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
 
-api_url = 'https://{dc}.api.mailchimp.com/3.0'.format(dc=MAILCHIMP_DATA_CENTER)
-members_endpoint = '{api_url}/lists/{list_id}/members'.format(
-    api_url=api_url,
-    list_id=MAILCHIMP_EMAIL_LIST_ID
-)
+# api_url = 'https://{dc}.api.mailchimp.com/3.0'.format(dc=MAILCHIMP_DATA_CENTER)
+# members_endpoint = '{api_url}/lists/{list_id}/members'.format(
+#     api_url=api_url,
+#     list_id=MAILCHIMP_EMAIL_LIST_ID
+# )
 
 
 def newsletter_sub(request):
