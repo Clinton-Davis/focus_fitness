86b6e71ebca091e2610debebcd600d15a22d4b1e
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-08-22 11:51:40 +0100

    settings in deployment mode

--- focus/settings.py
+++ focus/settings.py
@@ -2,10 +2,10 @@ import os
 import environ
 import dj_database_url
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-if DEBUG is False:
+if DEBUG is True:
     env = environ.Env()
     # read the .env file
     environ.Env.read_env()
@@ -30,7 +30,7 @@ if DEBUG is False:
     }
 
 
-if DEBUG is True:
+if DEBUG is False:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -114,7 +114,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'focus.wsgi.application'
 
 
-if DEBUG is True:
+if DEBUG is False:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
@@ -168,7 +168,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-if DEBUG is True:
+if DEBUG is False:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'focus-fitness'
@@ -214,7 +214,7 @@ CKEDITOR_CONFIGS = {
 }
 
 # stripe
-if DEBUG is True:
+if DEBUG is False:
     STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
     STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
     STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
@@ -227,7 +227,7 @@ if DEBUG is True:
     # SECURE_SSL_REDIRECT = True
     # # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-if DEBUG is False:
+if DEBUG is True:
     STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
     STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
     STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
