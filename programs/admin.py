40bc3b25e5b853f5c145632fa276d5b7fe78e7eb
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-09-02 11:04:04 +0100

    added new workouts and Workout admin

--- programs/admin.py
+++ programs/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Program, Workout
 
 
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'Program', 'position', )
+    list_filter = ('Program', 'position',)
+    list_editable = ('position',)
+
+
 admin.site.register(Program)
-admin.site.register(Workout)
+admin.site.register(Workout, WorkoutAdmin)
