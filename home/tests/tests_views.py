d32625f9e5bcce247e735447348f18eff287ccde
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-09-03 16:29:28 +0100

    added testing

--- home/tests/tests_views.py
+++ home/tests/tests_views.py
@@ -1,36 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from home.views import *
+from products.models import Product
 
 
-class TestHomeView(TestCase):
+class TestHomeViews(TestCase):
 
-    def test_index_view(self):
-        url = reverse('home')
-        resp = self.client.get(url)
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse('home')
+        self.about_url = reverse('about')
+        self.terms_url = reverse('terms')
+        self.privacy_url = reverse('privacy')
+        self.contact_url = reverse('contact')
+
+    def test_index_view_GET(self):
+        resp = self.client.get(self.index_url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name="home/index.html")
 
-    def test_about_view(self):
-        url = reverse('about')
-        resp = self.client.get(url)
+    def test_about_view_GET(self):
+        resp = self.client.get(self.about_url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name="home/about.html")
 
-    def test_terms_view(self):
-        url = reverse('terms')
-        resp = self.client.get(url)
+    def test_terms_view_GET(self):
+        resp = self.client.get(self.terms_url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name="home/terms.html")
 
-    def test_Privacy_view(self):
-        url = reverse('privacy')
-        resp = self.client.get(url)
+    def test_Privacy_view_GET(self):
+        resp = self.client.get(self.privacy_url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name="home/privacy.html")
 
-    def test_contact_view(self):
-        url = reverse('contact')
-        resp = self.client.get(url)
+    def test_contact_view_GET(self):
+        resp = self.client.get(self.contact_url)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name="home/contact.html")
