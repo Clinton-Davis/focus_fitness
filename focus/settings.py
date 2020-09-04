860e71fbd4d44266c9f48c18facf1e329f911a57
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-09-04 13:47:20 +0100

    Changed Debuf to false

--- focus/settings.py
+++ focus/settings.py
@@ -2,7 +2,7 @@ import os
 import environ
 import dj_database_url
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 if DEBUG is True:
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    # 'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.facebook',
 
     'crispy_forms',
     'ckeditor',
