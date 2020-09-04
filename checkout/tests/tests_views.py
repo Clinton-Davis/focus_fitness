8099fe3c0636fee46bff365b5dc499eb30138de0
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-09-04 13:39:34 +0100

    added new test to checkout

--- checkout/tests/tests_views.py
+++ checkout/tests/tests_views.py
@@ -1,1 +1,11 @@
 from django.test import TestCase, Client
+from django.urls import reverse
+from profiles.models import UserProfile
+
+
+class TestCheckoutViews(TestCase):
+
+    def test_get(self):
+        url = reverse('checkout:checkout')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
