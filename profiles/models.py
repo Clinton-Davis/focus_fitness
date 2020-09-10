a71e7c011be6c62c107000ae16ca8c3cf6335ce2
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-09-10 11:33:15 +0100

    added tests to checkout & profile forms

--- profiles/models.py
+++ profiles/models.py
@@ -8,7 +8,6 @@ from memberships.models import UserMembership
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(
--- profiles/tests/tests_forms.py
+++ profiles/tests/tests_forms.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from profiles.forms import UserProfileAddressForm
+from profiles.models import UserProfile
 
-# Create your tests here.
+
+class TestUserProfileAddressForm(TestCase):
+    """Testing that the nothing on the profile default is required"""
+
+    def test_user_profile_address_form_not_required(self):
+        valid_data = {
+            'default_email': '',
+            'default_phone_number': '',
+            'default_street_address1': '',
+            'default_street_address2': '',
+            'default_town_or_city': '',
+            'default_postcode': '',
+            'default_county': '',
+            'default_country': ''
+        }
+        form = UserProfileAddressForm(data=valid_data)
+        self.assertTrue(form.is_valid())
+        self.assertFalse(form.errors)
+
+    def test_user_profile_address_form_metaclass(self):
+        """ Testing the user filed is excluded in the profile UserProfileAddressForm"""
+        form = UserProfileAddressForm()
+        self.assertEqual(form.Meta.exclude, ('user',))
